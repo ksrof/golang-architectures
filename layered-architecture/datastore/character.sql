f9d2a4785c68b26abcd275187f088d13040eafda
ksrof <kevinsunercontacto@gmail.com>
2021-10-09 16:46:56 +0200

    changed single quotes to double quotes on INSERT

--- layered-architecture/datastore/character.sql
+++ layered-architecture/datastore/character.sql
@@ -13,5 +13,5 @@ CREATE TABLE characters(
   PRIMARY KEY(id)
 );
 
-INSERT INTO characters VALUES(1, 'Leeroy Jenkins', "70", "Male", "Humanoid", "Paladin", "Alliance")
-INSERT INTO characters VALUES(2, 'Sylvanas Windrunner', "??(Boss)", "Female", "Forsaken", "Ranger", "Horde")
+INSERT INTO characters VALUES(1, 'Leeroy Jenkins', '70', 'Male', 'Humanoid', 'Paladin', 'Alliance')
+INSERT INTO characters VALUES(2, 'Sylvanas Windrunner', '??(Boss)', 'Female', 'Forsaken', 'Ranger', 'Horde')
