3b18a8d5c76b59c25a65c0bb1d57aef737803b9c
ksrof <kevinsunercontacto@gmail.com>
2021-10-09 17:14:04 +0200

    corrected a typo on AUTO_INCREMENT and changed the db name from character to charactersdb

--- layered-architecture/datastore/character.sql
+++ layered-architecture/datastore/character.sql
@@ -1,9 +1,9 @@
-DROP DATABASE IF EXISTS character;
-CREATE DATABASE character;
-USE  character;
+DROP DATABASE IF EXISTS charactersdb;
+CREATE DATABASE charactersdb;
+USE charactersdb;
 
-CREATE TABLE characters(
-  id int NOT NULL AUTO_INCRMENT,
+CREATE TABLE characters (
+  id int NOT NULL AUTO_INCREMENT,
   name varchar(50),
   level varchar(50),
   gender varchar(50),
@@ -13,5 +13,5 @@ CREATE TABLE characters(
   PRIMARY KEY(id)
 );
 
-INSERT INTO characters VALUES(1, 'Leeroy Jenkins', '70', 'Male', 'Humanoid', 'Paladin', 'Alliance')
-INSERT INTO characters VALUES(2, 'Sylvanas Windrunner', '??(Boss)', 'Female', 'Forsaken', 'Ranger', 'Horde')
+INSERT INTO characters VALUES(1, 'Leeroy Jenkins', '70', 'Male', 'Humanoid', 'Paladin', 'Alliance');
+INSERT INTO characters VALUES(2, 'Sylvanas Windrunner', '??(Boss)', 'Female', 'Forsaken', 'Ranger', 'Horde');
