0c5a5fa62d41066e593db2c37ef533e55d24a470
ksrof <kevinsunercontacto@gmail.com>
2021-10-09 14:13:16 +0200

    added mysql configuration and connection

---
+++ layered-architecture/driver/mysql.go
@@ -0,0 +1,30 @@
+// layered-architecture/driver/mysql.go
+
+package driver
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type MySQLConfig struct {
+	Host 			string
+	User 			string
+	Password 	string
+	Port 			string
+	DB 				string
+}
+
+// MySQLConnect takes mysql config, forms the connection string and connects to mysql.
+func MySQLConnect(conf *MySQLConfig) (*sql.DB, error) {
+	connectionString := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v", conf.User, conf.Password, conf.Host, conf.Port, conf.DB)
+
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
