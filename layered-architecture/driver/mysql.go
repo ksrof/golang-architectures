b466aaceb13a0886ea41fe1acae65d6219fb6a61
ksrof <kevinsunercontacto@gmail.com>
2021-10-09 15:18:40 +0200

    removed pointer to MySQLConfig

--- layered-architecture/driver/mysql.go
+++ layered-architecture/driver/mysql.go
@@ -18,7 +18,7 @@ type MySQLConfig struct {
 }
 
 // MySQLConnect takes mysql config, forms the connection string and connects to mysql.
-func MySQLConnect(conf *MySQLConfig) (*sql.DB, error) {
+func MySQLConnect(conf MySQLConfig) (*sql.DB, error) {
 	connectionString := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v", conf.User, conf.Password, conf.Host, conf.Port, conf.DB)
 
 	db, err := sql.Open("mysql", connectionString)
