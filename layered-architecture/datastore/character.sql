f2c7e88ea163a20d1a9f60824eb20aac0da9cced
ksrof <kevinsunercontacto@gmail.com>
2021-10-09 13:47:34 +0200

    modeled the sql structure for the character database

---
+++ layered-architecture/datastore/character.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS character;
+CREATE DATABASE character;
+USE  character;
+
+CREATE TABLE characters(
+  id int NOT NULL AUTO_INCRMENT,
+  name varchar(50),
+  level varchar(50),
+  gender varchar(50),
+  race varchar(50),
+  class varchar(50),
+  faction varchar(50),
+  PRIMARY KEY(id)
+);
+
+INSERT INTO characters VALUES(1, 'Leeroy Jenkins', "70", "Male", "Humanoid", "Paladin")
+INSERT INTO characters VALUES(2, 'Sylvanas Windrunner', "??(Boss)", "Female", "Forsaken", "Ranger")
