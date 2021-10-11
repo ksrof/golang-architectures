d94c5b9062846519dd44e31007673f8cd318f968
ksrof <kevinsunercontacto@gmail.com>
2021-10-11 14:06:00 +0200

    added character usecase to communicate with the repository

---
+++ layered-gin/usecase/character/character.go
@@ -0,0 +1,50 @@
+// layered-gin/usecase/character/character.go
+
+package character
+
+import (
+	"layered-gin/entity"
+	"layered-gin/repository"
+	"log"
+)
+
+type CharacterUse struct {
+	repo repository.Character
+}
+
+func New(character repository.Character) CharacterUse {
+	return CharacterUse{repo: character}
+}
+
+// GetAll() returns all characters
+func (c CharacterUse) GetAll(characters []entity.Character) ([]entity.Character, error) {
+	resp, err := c.repo.GetAll(characters)
+	if err != nil {
+		log.Printf("could not get characters")
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+// GetByID() returns a specific character
+func (c CharacterUse) GetByID(id int) (entity.Character, error) {
+	resp, err := c.repo.GetByID(id)
+	if err != nil {
+		log.Printf("could not get character")
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+// Create() creates a character
+func (c CharacterUse) Create(character entity.Character) (entity.Character, error) {
+	resp, err := c.repo.Create(character)
+	if err != nil {
+		log.Printf("could create character")
+		return resp, err
+	}
+
+	return resp, nil
+}
