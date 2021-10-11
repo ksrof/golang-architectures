f95c684c428bf3ce7c833a3b3d6c850f5e06a6b4
ksrof <kevinsunercontacto@gmail.com>
2021-10-11 20:35:40 +0200

    changed param id int to id uint in GetByID function

--- layered-gin/usecase/use.go
+++ layered-gin/usecase/use.go
@@ -6,6 +6,6 @@ import "layered-gin/entity"
 
 type Character interface {
 	GetAll([]entity.Character) ([]entity.Character, error)
-	GetByID(id int) (entity.Character, error)
+	GetByID(id uint) (entity.Character, error)
 	Create(entity.Character) (entity.Character, error)
 }
