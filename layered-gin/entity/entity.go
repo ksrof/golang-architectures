2ad56928e39d0646e49929f7386c6041e207ac81
ksrof <kevinsunercontacto@gmail.com>
2021-10-11 13:10:43 +0200

    removed validate struct field tags

--- layered-gin/entity/entity.go
+++ layered-gin/entity/entity.go
@@ -6,11 +6,11 @@ import "gorm.io/gorm"
 
 type Character struct {
 	gorm.Model
-	Name 					string `gorm:"size:255;not null;unique" validate:"required,min=3,max=255" json:"name"` 
-	Title 				string `gorm:"size:255;not null" validate:"required,min=3,max=255" json:"title"`
-	Gender 				string `gorm:"size:100;not null" validate:"required,min=3,max=100" json:"gender"`
-	Race 					string `gorm:"size:100;not null" validate:"required,min=3,max=100" json:"race"`
-	Class 				string `gorm:"size:100;not null" validate:"required,min=3,max=100" json:"class"`
-	Affiliation 	string `gorm:"size:100;not null" validate:"required,min=3,max=100" json:"affiliation"`
-	Status 				string `gorm:"size:50;not null" validate:"required,min=3,max=50" json:"status"`
+	Name 					string `gorm:"size:255;not null;unique" json:"name"` 
+	Title 				string `gorm:"size:255;not null" json:"title"`
+	Gender 				string `gorm:"size:100;not null" json:"gender"`
+	Race 					string `gorm:"size:100;not null" json:"race"`
+	Class 				string `gorm:"size:100;not null" json:"class"`
+	Affiliation 	string `gorm:"size:100;not null" json:"affiliation"`
+	Status 				string `gorm:"size:50;not null" json:"status"`
 }
