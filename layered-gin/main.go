9d25819fa56c6f293d0902481363f90b4349495b
ksrof <kevinsunercontacto@gmail.com>
2021-10-12 11:57:56 +0200

    added main.go to load env, database and setup router to handle requests

---
+++ layered-gin/main.go
@@ -0,0 +1,56 @@
+// layered-gin/main.go
+
+package layeredgin
+
+import (
+	HandlerCharacter "layered-gin/delivery/character"
+	"layered-gin/driver"
+	character "layered-gin/repository/character"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	var err error
+
+	// Load .env config file	
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("unable to load .env config file")
+	}
+
+	// Database connection
+	conf := driver.DBConfig{
+		Host: os.Getenv("SQL_HOST"),
+		User: os.Getenv("SQL_USER"),
+		Pass: os.Getenv("SQL_PASS"),
+		Port: os.Getenv("SQL_PORT"),
+		Name: os.Getenv("SQL_NAME"),
+	}
+
+	db, err := driver.InitDB(conf)
+	if err != nil {
+		log.Fatalf("unable to connect to sql")
+	}
+
+	// Router setup to handle requests
+	repo := character.New(db)
+	handler := HandlerCharacter.New(repo)
+
+	router := gin.Default()
+
+	api := router.Group("/api") 
+	{
+		api.GET("/characters/find/all", handler.GetAll)
+		api.GET("/characters/find/:id", handler.GetByID)
+		api.POST("/characters/create", handler.Create)
+	}
+
+	err = router.Run(":8080")
+	if err != nil {
+		log.Fatalf("unable to start router")
+	}
+}
